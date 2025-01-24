b03080dcd8696ff6c0e48128ea3b1043d9069bfe
Pawarasasmina <pawarasasmina2002@icloud.com>
2025-01-24 16:53:03 +0530

    ghgyryfv

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 import logo from '../assets/logo2.png'; 
 
 const Footer = () => {
@@ -7,9 +7,7 @@ const Footer = () => {
       <div className="max-w-screen-xl mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
-          <img src={logo} alt="Logo" className="h-24" />  {/* Adjust the height as needed */}
-
-           
+            <img src={logo} alt="Logo" className="h-24" /> {/* Adjust the height as needed */}
           </div>
 
           <div className="flex space-x-4">
@@ -25,12 +23,18 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="mt-8 text-center text-sm">
-          <p>&copy; {new Date().getFullYear()} neuriOx IT. All rights reserved.</p>
+        <div className="mt-8 text-center text-black text-sm">
+        <p>
+            Design and Development by{' '}
+            <a href="https://www.neurioxit.systems/" className="hover:text-black">
+              neuriOx IT
+            </a>
+            .
+          </p>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
